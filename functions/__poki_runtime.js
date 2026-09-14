06bffe31b8e77a8dc3e93363b6bb192944178a9a
KKoma Programmer <ijunu3343@gmail.com>
2026-09-14 13:57:11 +0900

    Route authorized HTTPS requests through backend runtime

--- functions/__poki_runtime.js
+++ functions/__poki_runtime.js
@@ -9,8 +9,16 @@ export function onRequest() {
   const POKI_PREFIX = '/__poki_host/';
   const EXTERNAL_PREFIX = '/__external_host/';
   const upstream = tag.dataset.upstream || 'https://poki.com/';
-  const currentBase = tag.dataset.currentBase || PAGE_ORIGIN;
+  const currentBase = String(tag.dataset.currentBase || PAGE_ORIGIN).replace(/\/$/, '');
   const currentHost = String(tag.dataset.currentHost || '').toLowerCase();
+  let proxyMap = {};
+  try { proxyMap = JSON.parse(tag.dataset.proxyMap || '{}') || {}; } catch (_) {}
+  if (currentHost) proxyMap[currentHost] = currentBase;
+
+  function mappedBase(hostname) {
+    const host = String(hostname || '').toLowerCase();
+    return proxyMap[host] ? String(proxyMap[host]).replace(/\/$/, '') : '';
+  }
 
   function proxify(value) {
     if (value == null) return value;
@@ -23,13 +31,28 @@ export function onRequest() {
     if (u.origin === PAGE_ORIGIN) {
       if (u.pathname.startsWith(POKI_PREFIX) || u.pathname.startsWith(EXTERNAL_PREFIX)) return u.href;
       if (currentHost && currentHost !== 'poki.com') {
-        return String(currentBase).replace(/\/$/, '') + u.pathname + u.search + u.hash;
+        return currentBase + u.pathname + u.search + u.hash;
       }
       return u.href;
     }
 
-    if (String(u.hostname).toLowerCase() !== currentHost) return value;
-    return String(currentBase).replace(/\/$/, '') + u.pathname + u.search + u.hash;
+    if (u.protocol !== 'https:') return value;
+    const base = mappedBase(u.hostname);
+    if (!base) return value;
+    return base + u.pathname + u.search + u.hash;
+  }
+
+  function patchUrlSetter(proto, prop) {
+    try {
+      const desc = Object.getOwnPropertyDescriptor(proto, prop);
+      if (!desc || !desc.set || !desc.get) return;
+      Object.defineProperty(proto, prop, {
+        configurable: desc.configurable,
+        enumerable: desc.enumerable,
+        get: desc.get,
+        set(value) { return desc.set.call(this, proxify(value)); }
+      });
+    } catch (_) {}
   }
 
   const nativeFetch = window.fetch;
@@ -52,6 +75,13 @@ export function onRequest() {
     return nativeOpen.apply(this, args);
   };
 
+  if (navigator.sendBeacon) {
+    const nativeBeacon = navigator.sendBeacon.bind(navigator);
+    navigator.sendBeacon = function(url, data) {
+      return nativeBeacon(proxify(url), data);
+    };
+  }
+
   const attrs = new Set(['src', 'href', 'action', 'poster', 'data']);
   const nativeSetAttribute = Element.prototype.setAttribute;
   Element.prototype.setAttribute = function(name, value) {
@@ -59,6 +89,30 @@ export function onRequest() {
     if (attrs.has(lower)) value = proxify(value);
     return nativeSetAttribute.call(this, name, value);
   };
+
+  [
+    [HTMLImageElement, 'src'],
+    [HTMLScriptElement, 'src'],
+    [HTMLIFrameElement, 'src'],
+    [HTMLLinkElement, 'href'],
+    [HTMLAnchorElement, 'href'],
+    [HTMLFormElement, 'action'],
+    [HTMLSourceElement, 'src'],
+    [HTMLVideoElement, 'src'],
+    [HTMLVideoElement, 'poster'],
+    [HTMLAudioElement, 'src'],
+    [HTMLObjectElement, 'data']
+  ].forEach(([ctor, prop]) => {
+    if (ctor && ctor.prototype) patchUrlSetter(ctor.prototype, prop);
+  });
+
+  if (window.EventSource) {
+    const NativeEventSource = window.EventSource;
+    window.EventSource = function(url, config) {
+      return new NativeEventSource(proxify(url), config);
+    };
+    window.EventSource.prototype = NativeEventSource.prototype;
+  }
 })();`;
 
   return new Response(js, {
