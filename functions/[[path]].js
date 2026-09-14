2e7a3164142ea8f706d9a6969948a08ebc4c4561
KKoma Programmer <ijunu3343@gmail.com>
2026-09-14 16:58:46 +0900

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -56,7 +56,7 @@ function hostAllowed(hostname, patterns) {
       const root = pattern.slice(2);
       return host.endsWith(`.${root}`) && host !== root;
     }
-    return true;//host === pattern;
+    return host === pattern;
   });
 }
 
